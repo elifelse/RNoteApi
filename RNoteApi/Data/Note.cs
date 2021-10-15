a32af5daea9a96d89f3cc873f5cc7f8893fa8a7f
elifelse <elifgsavukcam@gmail.com>
2021-10-15 13:02:50 +0300

    Add project files.

---
+++ RNoteApi/Data/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RNoteApi.Data
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+        public DateTime? CreateTime { get; set; } = DateTime.Now;
+        public DateTime? ModifiedTime { get; set; }
+    }
+}
---
+++ RNoteApi/Migrations/20211015085025_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RNoteApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "Id", "Content", "CreateTime", "ModifiedTime", "Title" },
+                values: new object[] { 1, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", new DateTime(2021, 10, 15, 11, 50, 25, 2, DateTimeKind.Local).AddTicks(7831), null, "Sample Note 1" });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "Id", "Content", "CreateTime", "ModifiedTime", "Title" },
+                values: new object[] { 2, "Sed sit amet risus nec enim molestie consectetur.", new DateTime(2021, 10, 15, 11, 50, 25, 4, DateTimeKind.Local).AddTicks(5267), null, "Sample Note 2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
